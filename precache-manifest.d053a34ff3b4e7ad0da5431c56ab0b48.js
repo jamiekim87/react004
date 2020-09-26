78094f2da50d5151261ff95963fe42a2aebf2674
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-09-25 17:27:25 -0700

    Updates

---
+++ precache-manifest.d053a34ff3b4e7ad0da5431c56ab0b48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f22ca09f8e9587cee6f5758516f724",
+    "url": "/react004/index.html"
+  },
+  {
+    "revision": "6783bc3e32d6f1ff9cca",
+    "url": "/react004/static/js/2.112316ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react004/static/js/2.112316ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59d7ace176d2664351f4",
+    "url": "/react004/static/js/main.e4868dc1.chunk.js"
+  },
+  {
+    "revision": "915e07426dbd5cd779a7",
+    "url": "/react004/static/js/runtime-main.981728d7.js"
+  }
+]);
